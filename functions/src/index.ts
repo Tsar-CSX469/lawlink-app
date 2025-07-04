d71d0031bbd1dac89aac117d2a421922efb6e1de
hadithya <hhadithya34@gmail.com>
2025-07-04 13:12:02 +0530

    refactor: Update helloLawLink function to use onRequest for api testing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,3 @@
-// File: functions/src/index.ts
 // This file contains the main entry point for Firebase Cloud Functions.
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
@@ -7,9 +6,9 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 // Create a callable function with v1 API that should work with any firebase-tools version
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const helloLawLink = functions.https.onCall((data: any, context) => {
-  return {
-    message: `Hello ${data.name || "Anonymous"}! Welcome to LawLink.`,
-  };
+export const helloLawLink = functions.https.onRequest((req, res) => {
+  const name = req.body.name || "Annonymous";
+  res.json({
+    message: `Hello, ${name}! Welcome to LawLink!`,
+  });  
 });
