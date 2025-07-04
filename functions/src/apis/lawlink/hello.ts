b671c9b4ff2acf0bb5ef7403bc6ca646b1431ca7
hadithya <hhadithya34@gmail.com>
2025-07-04 14:41:41 +0530

    fix: Parse request body to convert string/buffer -> Json type

--- functions/src/apis/lawlink/hello.ts
+++ functions/src/apis/lawlink/hello.ts
@@ -2,8 +2,7 @@ import * as functions from "firebase-functions";
 import { generateWelcomeMessage } from "../../../services/messageService";
 
 export const helloLawLink = functions.https.onRequest((req, res) => {
-  const name = req.body.name || "Anonymous";
-  console.log('test' + req.body["name"]);
+  const name = JSON.parse(req.body).name || "Anonymous";
   const message = generateWelcomeMessage(name);
   
   res.json({
