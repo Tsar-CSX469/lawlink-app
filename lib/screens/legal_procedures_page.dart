28b4d44840a77b8677447d319edcfb2dc4b9907e
hadithya <hhadithya34@gmail.com>
2025-07-05 12:31:04 +0530

    refactor: minor changes

--- lib/screens/legal_procedures_page.dart
+++ lib/screens/legal_procedures_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:lawlink/screens/procedure_detail_page.dart';
+import 'package:lawlink/screens/Procedure_detail_page.dart';
 
 class LegalProceduresPage extends StatefulWidget {
   const LegalProceduresPage({Key? key}) : super(key: key);
